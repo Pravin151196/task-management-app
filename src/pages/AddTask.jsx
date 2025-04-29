36e28d8fb33d5a070599ff555ed59ee0aa6760e7
Pravin Kumar <pravinkumar2231@gmail.com>
2025-04-29 22:23:38 +0530

    Add frontend folder files

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,21 +1,13 @@
 import { useState } from 'react';
 import axios from '../services/api';
 import { useNavigate } from 'react-router-dom';
-import { Form, Button, Container, Row, Col, Card } from 'react-bootstrap';
+// import { Form, Button, Container, Row, Col, Card } from 'react-bootstrap';
 
 const AddTask = () => {
   const [taskName, setTaskName] = useState('');
   const [description, setDescription] = useState('');
   const [dueDate, setDueDate] = useState('');
   const navigate = useNavigate();
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   await axios.post('http://localhost:5000/api/tasks', { taskName, description, dueDate }, { 
-  //     withCredentials: true 
-  //   });
-  //   navigate('/');
-  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -34,8 +26,12 @@ const AddTask = () => {
 
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow rounded">
-      <h2 className="text-xl font-semibold mb-4">Add Task</h2>
+      <h2 className="text-xl font-semibold mb-4 px-5 text-muted">Add Task</h2>
+      <div className='p-5 m-3'>
+      <div className=' justify-content-center w-50'>
       <form onSubmit={handleSubmit}>
+        <div className='d-flex justify-content-between align-items-center mb-2'>
+        <label>Task Name:</label>
         <input
           className="w-full p-2 mb-3 border rounded"
           type="text"
@@ -44,12 +40,18 @@ const AddTask = () => {
           onChange={(e) => setTaskName(e.target.value)}
           required
         />
+        </div>
+        <div className='d-flex justify-content-between align-items-center mb-2'>
+          <label>Description:</label>
         <textarea
           className="w-full p-2 mb-3 border rounded"
           placeholder="Description"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         />
+        </div>
+        <div className='d-flex justify-content-between align-items-center mb2'>
+        <label>Date:</label>
         <input
           className="w-full p-2 mb-3 border rounded"
           type="date"
@@ -57,10 +59,15 @@ const AddTask = () => {
           onChange={(e) => setDueDate(e.target.value)}
           required
         />
-        <button className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700">
+        </div>
+        <div className='d-flex justify-content-end mt-2'>
+        <button className="btn btn-success">
           Create Task
         </button>
+        </div>
       </form>
+      </div>
+      </div>
     </div>
   );
 };
